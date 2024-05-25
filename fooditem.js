421c5aa82c82f3e4baa56f8c2e169da352e1d332
sanjayk19 <102471206+sanjayk19@users.noreply.github.com>
2024-05-25 17:20:54 +0530

    Add files via upload

---
+++ fooditem.js
@@ -0,0 +1,277 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Ambur Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 210,
+    img: 'images/biryani/Ambur-Chicken-Biryani.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Hyderabadi Biryani',
+    category : 'biryani',
+    rating : 4.2,
+    price: 250,
+    img: 'images/biryani/Chicken-Biryani-hyd.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Egg Biryani',
+    category : 'biryani',
+    rating : 3.9,
+    price: 120,
+    img: 'images/biryani/egg-biryani.jpeg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Fish Biryani',
+    category : 'biryani',
+    rating : 4.0,
+    price: 170,
+    img: 'images/biryani/goan-fish-biryani.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Mutton Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 270,
+    img: 'images/biryani/hyd-Mutton-Biryani.jpg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Kashmiri Biryani',
+    category : 'biryani',
+    rating : 4.7,
+    price: 150,
+    img: 'images/biryani/kashmiri.pulao.jpg',
+    quantity: 1
+},
+
+{
+    id: 10,
+    name: 'Chicken Roast',
+    category : 'chicken',
+    rating : 4.8,
+    price: 120,
+    img: 'images/chicken/Chicken_roast.jpg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Chicken Curry',
+    category : 'chicken',
+    rating : 4.3,
+    price: 160,
+    img: 'images/chicken/Chicken-Curry.jpg',
+    quantity: 1
+},
+
+{
+    id: 13,
+    name: 'Chicken Masala',
+    category : 'chicken',
+    rating : 3.4,
+    price: 170,
+    img: 'images/chicken/Chicken-Masala.jpeg',
+    quantity: 1
+},
+
+
+{
+    id: 17,
+    name: 'Palak Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 200,
+    img: 'images/paneer/palak-paneer.jpg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Paneer Butter Masala',
+    category : 'paneer',
+    rating : 4.3,
+    price: 200,
+    img: 'images/paneer/paneer-butter-masala.jpg',
+    quantity: 1
+
+},
+
+
+{
+    id: 21,
+    name: 'Paneer Lababdar',
+    category : 'paneer',
+    rating : 3.3,
+    price: 70,
+    img: 'images/paneer/paneer-lababdar.jpg',
+    quantity: 1
+},
+
+
+
+{
+    id: 25,
+    name: 'Veg Biriyani',
+    category : 'vegetable',
+    rating : 2.7,
+    price: 80,
+    img: 'images/vegetable/vegetable-biryani.jpg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Veg Curry',
+    category : 'vegetable',
+    rating : 4.8,
+    price: 100,
+    img: 'images/vegetable/vegetable-curry.jpeg',
+    quantity: 1
+},
+
+{
+    id: 28,
+    name: 'Veg Masala',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 100,
+    img: 'images/vegetable/vegetable-masala.jpg',
+    quantity: 1
+},
+
+{
+    id: 30,
+    name: 'Momos',
+    category : 'chinese',
+    rating : 4.9,
+    price: 180,
+    img: 'images/chinese/cabbage-momos-.jpg',
+    quantity: 1
+},
+{
+    id: 31,
+    name: 'Chicken Manchurian',
+    category : 'chinese',
+    rating : 4.3,
+    price: 160,
+    img: 'images/chinese/ChickenManchurian.jpg',
+    quantity: 1
+},
+{
+    id: 32,
+    name: 'Chilly Chicken',
+    category : 'chinese',
+    rating : 3.3,
+    price: 100,
+    img: 'images/chinese/Chili-Chicken.jpg',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'Spring Roll',
+    category : 'chinese',
+    rating : 4.3,
+    price: 120,
+    img: 'images/chinese/spring-rolls.jpg',
+    quantity: 1
+},
+{
+    id: 35,
+    name: 'Szechuan Chicken',
+    category : 'chinese',
+    rating : 4.7,
+    price: 100,
+    img: 'images/chinese/szechuan-chicken.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Fried Rice',
+    category : 'chinese',
+    rating : 4.0,
+    price: 80,
+    img: 'images/chinese/veg-fried-rice.jpg',
+    quantity: 1
+},
+{
+    id: 37,
+    name: 'Butter Masala Dosa',
+    category : 'south indian',
+    rating : 4.9,
+    price: 180,
+    img: 'images/south indian/Butter-Masala-Dosa.png',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'Idli',
+    category : 'south indian',
+    rating : 3.8,
+    price: 200,
+    img: 'images/south indian/idli-with-rice-flour.jpg',
+    quantity: 1
+},
+{
+    id: 39,
+    name: 'Masala Dosa',
+    category : 'south indian',
+    rating : 4.2,
+    price: 120,
+    img: 'images/south indian/masala-dosa.jpg',
+    quantity: 1
+},
+{
+    id: 40,
+    name: 'Bonda',
+    category : 'south indian',
+    rating : 4.7,
+    price: 100,
+    img: 'images/south indian/mysore-bonda.jpg',
+    quantity: 1
+},
+{
+    id: 41,
+    name: 'Onion Uttapam',
+    category : 'south indian',
+    rating : 4.0,
+    price: 150,
+    img: 'images/south indian/onion-uttapam.jpg',
+    quantity: 1
+},
+{
+    id: 42,
+    name: 'Plain Dosa',
+    category : 'south indian',
+    rating : 2.9,
+    price: 400,
+    img: 'images/south indian/plain-dosa.jpeg',
+    quantity: 1
+},
+{
+    id: 43,
+    name: 'Rava Uttapam',
+    category : 'south indian',
+    rating : 4.1,
+    price: 250,
+    img: 'images/south indian/Rava-Uttapam.jpg',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'Sambhar Vada',
+    category : 'south indian',
+    rating : 4.7,
+    price: 340,
+    img: 'images/south indian/sambhar-vada.jpg',
+    quantity: 1
+},
+]
+
+export {foodItem};
